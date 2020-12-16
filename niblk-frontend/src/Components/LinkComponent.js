614e5ef4eea49e37493dc5bf4af35c05cf45d3ba
ManoranjanMaharana1 <1705415@kiit.ac.in>
2020-12-16 15:52:02 +0530

    ✨CSS Updated!

--- niblk-frontend/src/Components/LinkComponent.js
+++ niblk-frontend/src/Components/LinkComponent.js
@@ -2,16 +2,24 @@ import React from 'react';
 
 const LinkComponent = () => {
     return (
-        <div className="lower">
+        <footer className="lower">
             <div className="longURL-box">
                 <form >
-                    <input type="text" id="longtextbox" name="longurl" value="longurl" />
-                    
-                    
+                    <input type="text" autocomplete="off" id="longtextbox" name="longurl" placeholder="LongURL"/>
+                    <input type="submit" className="submit-btn" value="Go"/>
                 </form>
             </div>
+            <div className="shortURL-box">
+                <div className="label">
+                <input type="radio"  id="custom" name="isCustom" /> <label id="customLabel">Custom URL</label></div>
+                <input type="text" autocomplete="off" id="shorttextbox" name="shorturl" placeholder="ShortURL" className="display"/>
+                <div className="result-url display">
+                    <h1>nib.lk/QWe23rth</h1>
+                    <img src="./copy.svg" alt="" id="copy" width="40px"/>
+                </div>
+            </div>
 
-        </div>
+        </footer>
     )
 }
 export default LinkComponent;
